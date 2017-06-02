be0277e80ff1bff1827f63fc266524ebaa0a2c52
Kamil Podlasek <admin@kamilpodlasek.pl>
2017-06-02 14:08:58 +0200

    Created add profile form and table

---
+++ src/components/CreateProfile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CreateProfile = ({createProfile}) => {
+  let nameNode, carrotsNode;
+
+  const handleSubmit = function(e) {
+    e.preventDefault();
+
+    const name = nameNode.value;
+    const carrots = carrotsNode.value;
+
+    if(name && carrots) {
+      createProfile({name, carrots});
+    }
+  }
+
+  return (
+    <div>
+      <h2>Add profile:</h2>
+      <form onSubmit={handleSubmit}>
+        <p>
+          Name:
+          <input type="text" name="name" ref={node => nameNode = node} />
+        </p>
+        <p>
+          Carrots:
+          <input type="number" name="carrots" ref={node => carrotsNode = node}  />
+        </p>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
+
+export default CreateProfile;
--- src/reducers/profileReducer.js
+++ src/reducers/profileReducer.js
@@ -4,6 +4,13 @@ export default function(state = [], action) {
   switch(action.type) {
     case types.LOAD_PROFILES: return [...state, ...action.profiles];
     case types.CREATE_PROFILE: return [...state, action.profile];
+    case types.DELETE_PROFILE: return state.filter(profile => profile.id !== action.id);
+    case types.MANIPULATE_CARROTS: return state.map(profile => {
+      if(profile.id === action.id) {
+        profile.carrots = action.carrots;
+      }
+      return profile;
+    });
     default: return state;
   }
 }
